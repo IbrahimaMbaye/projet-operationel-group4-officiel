98d7343e6adf980946f072740d1cdb2fbf1df249
IbrahimaMbaye <125671579+IbrahimaMbaye@users.noreply.github.com>
2023-09-09 10:33:15 +0200

    add files done

---
+++ product.php
@@ -0,0 +1,128 @@
+<?php
+  $page_title = 'All Product';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(2);
+  $products = join_product_table();
+?>
+<?php include_once('layouts/header.php'); ?>
+  <div class="row">
+     <div class="col-md-12">
+       <?php echo display_msg($msg); ?>
+     </div>
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+         <div class="pull-right">
+         <div>
+            
+            <h2>search bar using php</h2>
+            <form method="post">
+              <label>Search</label>
+              <input type="text" name="search">
+              <input type="submit" name="submit"> 
+            </form>
+            <?php
+$con = new PDO("mysql:host=localhost;dbname=inventory_system",'root','');
+
+if (isset($_POST["submit"])){
+    $str = $_POST["search"];
+    $sth = $con->prepare("SELECT * FROM `products` WHERE name ='$str'");
+    $sth->setFetchMode(PDO:: FETCH_OBJ);
+    $sth ->execute();
+
+    if($row = $sth->fetch())
+    {
+        ?>
+        <br><br><br>
+        <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th class="text-center" style="width: 50px;">Name</th>
+                <th class="text-center" style="width: 10%;"> quantity</th>
+                <th class="text-center" style="width: 10%;"> buy price </th>
+                <th class="text-center" style="width: 10%;"> sale price </th>
+                <th class="text-center" style="width: 10%;"> Categories </th>
+                <th class="text-center" style="width: 10%;"> Media Id </th>
+                <th class="text-center" style="width: 10%;"> Date </th>
+              </tr>
+            </thead>
+     
+         <td><?php echo $row->name; ?></td>
+         <br>
+         <td><?php echo $row->quantity; ?></td>
+         <br>
+         <td><?php echo $row->buy_price; ?></td>
+         <br>
+         <td><?php echo $row->sale_price; ?></td>
+         <br>
+         <td><?php echo $row->categorie_id; ?></td>
+         <br>
+         <td><?php echo $row->media_id; ?></td>
+         <br>
+         <td><?php echo $row->date; ?></td>
+     </tr>
+        </table>
+        <?php
+    }
+    
+    else{
+        echo  "Name Does not exist";
+    }
+}
+?>
+          </div>
+           <a href="add_product.php" class="btn btn-primary">Add New</a>
+         </div>
+        </div>
+        <div class="panel-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th class="text-center" style="width: 50px;">#</th>
+                <th> Photo</th>
+                <th> Product Title </th>
+                <th class="text-center" style="width: 10%;"> Categories </th>
+                <th class="text-center" style="width: 10%;"> In-Stock </th>
+                <th class="text-center" style="width: 10%;"> Buying Price </th>
+                <th class="text-center" style="width: 10%;"> Selling Price </th>
+                <th class="text-center" style="width: 10%;"> Product Added </th>
+                <th class="text-center" style="width: 100px;"> Actions </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($products as $product):?>
+              <tr>
+                <td class="text-center"><?php echo count_id();?></td>
+                <td>
+                  <?php if($product['media_id'] === '0'): ?>
+                    <img class="img-avatar img-circle" src="uploads/products/no_image.png" alt="">
+                  <?php else: ?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $product['image']; ?>" alt="">
+                <?php endif; ?>
+                </td>
+                <td> <?php echo remove_junk($product['name']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['quantity']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['buy_price']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['sale_price']); ?></td>
+                <td class="text-center"> <?php echo read_date($product['date']); ?></td>
+                <td class="text-center">
+                  <div class="btn-group">
+                    <a href="edit_product.php?id=<?php echo (int)$product['id'];?>" class="btn btn-info btn-xs"  title="Edit" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-edit"></span>
+                    </a>
+                    <a href="delete_product.php?id=<?php echo (int)$product['id'];?>" class="btn btn-danger btn-xs"  title="Delete" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-trash"></span>
+                    </a>
+                  </div>
+                </td>
+              </tr>
+             <?php endforeach; ?>
+            </tbody>
+          </tabel>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include_once('layouts/footer.php'); ?>
